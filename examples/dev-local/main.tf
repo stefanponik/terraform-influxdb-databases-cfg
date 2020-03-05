51e14f4051c512afef01a8216167ef30e700662f
Stefan Ponik <stefanponik@gmail.com>
2020-03-05 17:56:42 +0000

    Update main.tf example

--- examples/dev-local/main.tf
+++ examples/dev-local/main.tf
@@ -17,10 +17,10 @@ provider "influxdb" {
 # Modules
 #########
 
-#module "terraform-influxdb-databases-cfg" {
-#  source          = "github.com/stefanponik/terraform-influxdb-databases-cfg.git?ref=v0.0.1"
-#  database_schema = var.database_schema
-#  providers = {
-#    influxdb = influxdb
-#  }
-#}
+module "terraform-influxdb-databases-cfg" {
+  source          = "github.com/stefanponik/terraform-influxdb-databases-cfg.git?ref=v0.0.2"
+  database_schema = var.database_schema
+  providers = {
+    influxdb = influxdb
+  }
+}
